21d0e966f64b5440877a1f3a239007a5b65c8e3d
Nikola Madzharov <nmadzharov1912@gmail.com>
2022-09-16 23:30:26 +0300

    Created MobilePhone button and added view

---
+++ MyWebProject/Controllers/MobilePhoneController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebProject.Controllers
+{
+    public class MobilePhoneController : Controller
+    {
+        // GET: MobilePhoneController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
