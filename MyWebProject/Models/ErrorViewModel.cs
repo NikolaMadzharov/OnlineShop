d1a81a43ccc037c046cf0459f9eea046b6e0b020
Nikola Madzharov <89745007+koko1912@users.noreply.github.com>
2022-10-04 20:06:34 +0300

    Delete MyWebProject directory

--- MyWebProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyWebProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- MyWebProject/Models/MobilePhones/MobilePhoneViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyWebProject.Models.MobilePhones
-{
-    public class MobilePhoneViewModel
-    {
-        public int id { get; set; }
-
-        public string Model { get; set; }
-    }
-}
